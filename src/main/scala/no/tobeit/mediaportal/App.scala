18f364fd722874d2f4ee112051ea1440cbc36a76
Runar Bell <runar@jpro.no>
2010-07-10 00:20:41 +0200

    made from netbeans

---
+++ src/main/scala/no/tobeit/mediaportal/App.scala
@@ -0,0 +1,9 @@
+package no.tobeit.mediaportal
+
+/**
+ * Hello world!
+ *
+ */
+object App extends Application {
+  println( "Hello World!" )
+}
---
+++ src/test/scala/no/tobeit/mediaportal/AppTest.scala
@@ -0,0 +1,17 @@
+package no.tobeit.mediaportal
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
