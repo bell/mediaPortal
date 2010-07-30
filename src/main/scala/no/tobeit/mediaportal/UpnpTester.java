c2111e3bf485645c3284bcf18137511e1615fd2c
Mikael Tollefsen <mikael@tollefsen.biz>
2010-07-31 00:02:07 +0200

    Added new upnp test to create hole i firewall

--- src/main/scala/no/tobeit/mediaportal/UpnpTester.java
+++ src/main/scala/no/tobeit/mediaportal/UpnpTester.java
@@ -1,24 +1,21 @@
 package no.tobeit.mediaportal;
 
 import org.teleal.cling.*;
+import org.teleal.cling.controlpoint.ActionCallback;
+import org.teleal.cling.model.action.ActionException;
 import org.teleal.cling.model.action.ActionInvocation;
+import org.teleal.cling.model.message.UpnpResponse;
 import org.teleal.cling.model.message.header.STAllHeader;
-import org.teleal.cling.model.meta.DeviceDetails;
-import org.teleal.cling.model.meta.DeviceService;
-import org.teleal.cling.model.meta.RemoteDevice;
-import org.teleal.cling.model.meta.Service;
-import org.teleal.cling.model.types.ServiceId;
-import org.teleal.cling.model.types.UDAServiceId;
+import org.teleal.cling.model.meta.*;
+import org.teleal.cling.model.types.*;
 import org.teleal.cling.registry.DefaultRegistryListener;
 import org.teleal.cling.registry.Registry;
 import org.teleal.cling.registry.RegistryListener;
-import org.teleal.cling.transport.Router;
 
 
 public class UpnpTester implements Runnable {
     public void run() {
         UpnpService upnpService = new UpnpServiceImpl();
-
         // Add a listener for device registration events
         upnpService.getRegistry().addListener(
                 createRegistryListener(upnpService)
@@ -26,43 +23,88 @@ public class UpnpTester implements Runnable {
 
 
         // Broadcast a search message for all devices
-        upnpService.getControlPoint().search(
-                new STAllHeader()
-        );
+        upnpService.getControlPoint().search(new STAllHeader());
+
     }
 
 
     RegistryListener createRegistryListener(final UpnpService upnpService) {
         return new DefaultRegistryListener() {
 
-            ServiceId serviceId = new UDAServiceId("SwitchPower");
+            ServiceId serviceId = new UDAServiceId("WANIPConn1");
 
             @Override
             public void remoteDeviceAdded(Registry registry, RemoteDevice device) {
-                System.out.println("Device: " + device.getDisplayString());
-                DeviceDetails dd = device.getDetails();
-                System.out.println("BaseURL: " + dd.getBaseURL());
-                DeviceService switchPower;
-                if ((switchPower = device.findDeviceService(serviceId)) != null) {
-                    System.out.println("Service discovered: " + switchPower);
-                    executeAction(upnpService, switchPower.getService());
+                DeviceService wanipconn;
+                if ((wanipconn = device.findDeviceService(serviceId)) != null) {
+                    System.out.println("Service discovered: " + wanipconn);
+                    executeAction(upnpService, wanipconn.getService());
                 }
 
             }
 
             @Override
             public void remoteDeviceRemoved(Registry registry, RemoteDevice device) {
-                DeviceService switchPower;
-                if ((switchPower = device.findDeviceService(serviceId)) != null) {
-                    System.out.println("Service disappeared: " + switchPower);
+                DeviceService wanipconn;
+                if ((wanipconn = device.findDeviceService(serviceId)) != null) {
+                    System.out.println("Service disappeared: " + wanipconn);
                 }
             }
 
         };
     }
 
-    void executeAction(UpnpService upnpService, Service switchPowerService) {
-    }         
+    void executeAction(UpnpService upnpService, Service wanip) {
+        ActionInvocation addPortMappingInvocation =  new AddPortMappingActionInvocation(wanip);
+        // Executes asynchronous in the background
+        upnpService.getControlPoint().execute(
+                new ActionCallback(addPortMappingInvocation) {
+                    public void success(ActionInvocation actionInvocation) {
+                        assert actionInvocation.getOutput().getValues().length == 0;
+                        System.out.println("Successfully called action!");
+                    }
+
+                    public void failure(ActionInvocation actionInvocation, UpnpResponse operation) {
+                        System.out.println("Failed action");
+                        System.err.println(createDefaultFailureMessage(actionInvocation, operation));
+                    }
+                }
+        );
+    }
+
+
+    class AddPortMappingActionInvocation extends ActionInvocation {
+        AddPortMappingActionInvocation(Service service) {
+            super(service.getAction("AddPortMapping"));
+            try {
+                // This might throw an ActionException if the value is of wrong type
+                getInput().addValue(null);                              // NewRemoteHost
+                getInput().addValue(new UnsignedIntegerTwoBytes(1234)); // NewExternalPort
+                getInput().addValue("TCP");                             // NewProtocol
+                getInput().addValue(new UnsignedIntegerTwoBytes(1234)); // NewInternalPort
+                getInput().addValue("192.168.1.112");                   // NewInternalClient
+                getInput().addValue(true);                              // NewEnabled
+                getInput().addValue("Description");                     // NewPortMappingDescription
+                getInput().addValue(new UnsignedIntegerFourBytes(0));   // NewLeaseDuration
+            } catch (ActionException ex) {
+                System.err.println(ex.getMessage());
+            }
+        }
+    }
+
+    class DeletePortMappingActionInvocation extends ActionInvocation {
+        DeletePortMappingActionInvocation(Service service) {
+            super(service.getAction("DeletePortMapping"));
+            try {
+                // This might throw an ActionException if the value is of wrong type
+                getInput().addValue(null);                              // NewRemoteHost
+                getInput().addValue(new UnsignedIntegerTwoBytes(1234)); // NewExternalPort
+                getInput().addValue("TCP");                             // NewProtocol
+            } catch (ActionException ex) {
+                System.err.println(ex.getMessage());
+            }
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         Thread client = new Thread(new UpnpTester());
