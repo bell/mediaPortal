d72df92fbfc2e2074c83d6dad23cbc831bcd8625
Mikael Tollefsen <mikael@tollefsen.biz>
2010-08-15 11:17:16 +0200

    Firsh shot at getting correct ip under linux

--- src/main/scala/no/tobeit/mediaportal/UpnpTester.java
+++ src/main/scala/no/tobeit/mediaportal/UpnpTester.java
@@ -1,5 +1,15 @@
 package no.tobeit.mediaportal;
 
+import java.net.DatagramSocket;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.teleal.cling.*;
 import org.teleal.cling.controlpoint.ActionCallback;
 import org.teleal.cling.model.action.ActionException;
@@ -30,23 +40,22 @@ public class UpnpTester implements Runnable {
 
     RegistryListener createRegistryListener(final UpnpService upnpService) {
         return new DefaultRegistryListener() {
-
             ServiceId serviceId = new UDAServiceId("WANIPConn1");
 
             @Override
             public void remoteDeviceAdded(Registry registry, RemoteDevice device) {
-                DeviceService wanipconn;
-                if ((wanipconn = device.findDeviceService(serviceId)) != null) {
+                RemoteService wanipconn;
+                if ((wanipconn = device.findService(serviceId)) != null) {
                     System.out.println("Service discovered: " + wanipconn);
-                    executeAction(upnpService, wanipconn.getService());
+                    executeAction(upnpService, wanipconn);
                 }
 
             }
 
             @Override
             public void remoteDeviceRemoved(Registry registry, RemoteDevice device) {
-                DeviceService wanipconn;
-                if ((wanipconn = device.findDeviceService(serviceId)) != null) {
+                RemoteService wanipconn;
+                if ((wanipconn = device.findService(serviceId)) != null) {
                     System.out.println("Service disappeared: " + wanipconn);
                 }
             }
@@ -82,7 +91,9 @@ public class UpnpTester implements Runnable {
                 getInput().addValue(new UnsignedIntegerTwoBytes(1234)); // NewExternalPort
                 getInput().addValue("TCP");                             // NewProtocol
                 getInput().addValue(new UnsignedIntegerTwoBytes(1234)); // NewInternalPort
-                getInput().addValue("192.168.1.112");                   // NewInternalClient
+                String localhost = getLocalhost();
+                getInput().addValue(localhost);                         // NewInternalClient
+                System.out.println("Adding port to : " + localhost);
                 getInput().addValue(true);                              // NewEnabled
                 getInput().addValue("Description");                     // NewPortMappingDescription
                 getInput().addValue(new UnsignedIntegerFourBytes(0));   // NewLeaseDuration
@@ -105,6 +116,42 @@ public class UpnpTester implements Runnable {
             }
         }
     }
+
+    public InetAddress getLocalHost(InetAddress intendedDestination)
+            throws SocketException {
+            DatagramSocket sock = new DatagramSocket(39485345);
+        sock.connect(intendedDestination, 39485345);
+        return sock.getLocalAddress();
+    }
+
+    public static String getLocalhost() {
+        String retVal = "127.0.0.1";
+        try {
+            Enumeration<NetworkInterface> netInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (netInterfaces.hasMoreElements()) {
+                NetworkInterface ni = netInterfaces.nextElement();
+                System.out.println(ni.getName());
+                InetAddress ip = null;
+                for(InterfaceAddress ip4 : ni.getInterfaceAddresses()) {
+                    if(ip4.getAddress() instanceof Inet4Address) {
+                        ip = ip4.getAddress();
+                    }
+                }
+
+                if (ip != null && !ip.isLoopbackAddress() && ip.getHostAddress().indexOf(":") == -1) {
+                    System.out.println("Interface " + ni.getName() + " seems to be InternetInterface. I'll take it...");
+                    retVal = ni.getName();
+                    break;
+                } 
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(UpnpTester.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return retVal;
+    }
+
+
 
     public static void main(String[] args) throws Exception {
         Thread client = new Thread(new UpnpTester());
